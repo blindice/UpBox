659ec9c3d65ecc5022ce849ed0eb0c89b9e5ad43
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-27 15:52:36 +0800

    added donwload to file controller

--- UpBox/Controllers/FileController.cs
+++ UpBox/Controllers/FileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,7 @@ using UpBox.Service;
 
 namespace UpBox.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class FileController : ControllerBase
     {
@@ -24,7 +25,8 @@ namespace UpBox.Controllers
             _config = config;
             _svc = svc;
         }
-        [HttpGet]
+
+        [HttpGet("getall")]
         public async Task<IActionResult> GetAll()
         {
             var files = await _svc.GetAllFilesAsync();
@@ -32,24 +34,31 @@ namespace UpBox.Controllers
             return Ok(files);
         }
 
-        [HttpGet]
+        [HttpGet("getbyfiletypeandname")]
         public async Task<IActionResult> GetByFileTypeAndName([FromQuery] string fileName, [FromQuery] int? fileType)
         {
             var files = await _svc.GetFilesByNameAndFileTypeAsync(fileName, fileType);
             return Ok(files);
         }
 
-        [HttpPost]
+        [HttpPost("upload")]
         [DisableRequestSizeLimit]
         public async Task<IActionResult> Upload([FromForm] FileUploadDTO file)
         {
-
-            if (file.File.Length > 0) await _svc.Upload(file);
+            if (file.File.Length > 0) await _svc.UploadAsync(file);
 
             return Ok();
         }
 
-        [HttpPost("{id}")]
+        [HttpGet("download")]
+        public async Task<IActionResult> Download([FromQuery] string fileName)
+        {
+            (byte[] bytes, string contentType, string filePath) = await _svc.DownloadAsync(fileName);
+
+            return File(bytes, contentType, Path.GetFileName(filePath));
+        }
+
+        [HttpPost("delete/{id}")]
         public async Task<IActionResult> Delete(int? id, [FromBody] FileUpdateDTO file)
         {
             await _svc.DeleteFileAsync((int)id, file);
