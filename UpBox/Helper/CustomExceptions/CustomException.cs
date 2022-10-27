07a436ee90b24c89daf7768082226a4aea9a82e0
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-28 07:53:50 +0800

    added CustomException

---
+++ UpBox/Helper/CustomExceptions/CustomException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpBox.Helper.CustomExceptions
+{
+    public class CustomException : Exception
+    {
+        public CustomException()
+        {
+        }
+
+        public CustomException(string message) : base(message)
+        {
+        }
+    }
+}
