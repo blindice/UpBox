24ddbf6f688991c95b5ee1e63922c0dea076e822
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-28 07:53:43 +0800

    added ResponseDTO

---
+++ UpBox/DTO/ResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace UpBox.DTO
+{
+    public class ResponseDTO<T>
+    {
+        public bool isSuccess { get; set; }
+
+        public T Result { get; set; }
+
+        public string Message { get; set; }
+
+        public int? ErrorCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
