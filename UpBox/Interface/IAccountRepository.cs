5afe075e8a1b25ac47b325cb97233c6345a44f88
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-28 09:22:35 +0800

    added account repo

---
+++ UpBox/Interface/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using UpBox.Model;
+
+namespace UpBox.Interface
+{
+    public interface IAccountRepository
+    {
+        IQueryable<tbl_user> GetByCondition(Expression<Func<tbl_user, bool>> expression);
+    }
+}
---
+++ UpBox/Repository/AccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using UpBox.Interface;
+using UpBox.Model;
+using UpBox.Model.Context;
+
+namespace UpBox.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        UpBoxContext _context;
+        public AccountRepository(UpBoxContext context) => _context = context;
+
+        public IQueryable<tbl_user> GetByCondition(Expression<Func<tbl_user, bool>> expression) => _context.tbl_users.Where(expression).AsNoTracking();
+    }
+}
