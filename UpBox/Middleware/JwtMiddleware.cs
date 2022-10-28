5f8bda01c2754cc36440316424ceb5da8d99880e
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-28 09:22:50 +0800

    added jwt middleware

---
+++ UpBox/Middleware/JwtMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpBox.Interface;
+
+namespace UpBox.Middleware
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public JwtMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context, IAccountService service)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+                attachAccountToContext(context, token, service);
+
+            await _next(context);
+        }
+
+        private void attachAccountToContext(HttpContext context, string token, IAccountService service)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidIssuer = _configuration["Jwt:Issuer"],
+                ValidAudience = _configuration["Jwt:Audience"],
+                // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                ClockSkew = TimeSpan.Zero
+            }, out SecurityToken validatedToken);
+
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            var accountId = jwtToken.Claims.First(x => x.Type == "id").Value;
+
+            if (string.IsNullOrEmpty(accountId)) throw new BadHttpRequestException("Error Attaching Account to Context");
+
+            // attach account to context on successful jwt validation
+            context.Items["User"] = service.GetUserByUserIdAsync(Convert.ToInt32(accountId));
+        }
+    }
+}
