7656b1ce871c0a19c1758bff91ea7674f21aa4bf
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-28 09:23:21 +0800

    added account controller

--- UpBox/Controllers/AccountController.cs
+++ UpBox/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UpBox.DTO;
+using UpBox.Interface;
 
 namespace UpBox.Controllers
 {
@@ -11,5 +14,39 @@ namespace UpBox.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        IAccountService _service;
+        public AccountController(IAccountService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginDTO account)
+        {
+            if (account is null) return BadRequest("Invalid Account!");
+
+            if (!ModelState.IsValid) return BadRequest("Invalid Account");
+
+            var result = await _service.VerifyUserAsync(account);
+
+            if (result is null) return NotFound("Account Not Found!");
+
+            var tokenString = await _service.GenerateJWTTokenAsync(result);
+
+            return Ok(new { Token = tokenString });
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO account)
+        {
+            if (!ModelState.IsValid) return BadRequest("Invalid Account");
+
+
+
+            return Ok();
+        }
+
     }
 }
