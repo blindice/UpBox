8cf86f1f0fcc95cb9c1e8ae248c8d3734cf8235e
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-28 09:23:09 +0800

    added helper class passwordmanager

---
+++ UpBox/Helper/PasswordManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpBox.Helper
+{
+    public class PasswordManager
+    {
+        const int SALT_SIZE = 24;
+
+        public static string GetHash(string password, string salt)
+        {
+            var hashedPassword = HashSha512(password, salt);
+
+            return hashedPassword;
+        }
+
+        public static string GenerateSalt()
+        {
+            var buff = new byte[SALT_SIZE];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(buff);
+            }
+            return Convert.ToBase64String(buff);
+        }
+
+        static string HashSha512(string rawData, string salt)
+        {
+            // Create a SHA256. SHA = Secure Hash Algorithm.
+            using (var sha512Hash = SHA512.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = sha512Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData + salt));
+
+                // Convert byte array to a string
+                var builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString().ToUpper();
+            }
+        }
+    }
+}
