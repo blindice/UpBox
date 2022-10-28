5eb932b107c4cfcf672aec24dba62628e0880286
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-10-28 09:49:45 +0800

    added account profile

---
+++ UpBox/Profiles/AccountProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpBox.DTO;
+using UpBox.Model;
+
+namespace UpBox.Profiles
+{
+    public class AccountProfile : Profile
+    {
+        public AccountProfile()
+        {
+            CreateMap<tbl_user, UserInfoDTO>().ReverseMap();
+        }
+    }
+}
