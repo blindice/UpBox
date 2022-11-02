3070591cb6faeec29a3f50dfaba49cff27b165f7
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-11-02 16:40:32 +0800

    frontend added files page

--- UpBox/ClientApp/src/components/Files.js
+++ UpBox/ClientApp/src/components/Files.js
@@ -1,5 +1,60 @@
 import React from "react";
+import { AwesomeButton } from "react-awesome-button";
+import {
+  VideoCameraOutlined,
+  FileTextOutlined,
+  SoundOutlined,
+  FileImageOutlined,
+} from "@ant-design/icons";
+
+import "./Files.css";
+import "react-awesome-button/dist/styles.css";
 
 export default function Files() {
-  return <h1>Files</h1>;
+  return (
+    <>
+      <h4 className="header-text">Files</h4>
+      <div className="awesome-button-container">
+        <AwesomeButton
+          className="aws"
+          type="primary"
+          style={{ height: 150, width: 650, fontSize: "50px" }}
+          before={<VideoCameraOutlined style={{ marginRight: "20px" }} />}
+        >
+          Videos
+        </AwesomeButton>
+        <AwesomeButton
+          className="aws"
+          type="primary"
+          style={{ height: 150, width: 650, fontSize: "50px" }}
+          before={<FileImageOutlined style={{ marginRight: "20px" }} />}
+        >
+          Documents
+        </AwesomeButton>
+        <AwesomeButton
+          className="aws"
+          type="primary"
+          style={{ height: 150, width: 650, fontSize: "50px" }}
+          before={<FileTextOutlined style={{ marginRight: "20px" }} />}
+        >
+          Audio
+        </AwesomeButton>
+        <AwesomeButton
+          className="aws"
+          type="primary"
+          style={{ height: 150, width: 650, fontSize: "50px" }}
+          before={<SoundOutlined style={{ marginRight: "20px" }} />}
+        >
+          Image
+        </AwesomeButton>
+      </div>
+      {/* <AwesomeButton
+        type="primary"
+        style={{ height: 100, width: 500, fontSize: "100px" }}
+        before={<InboxOutlined />}
+      >
+        Primary
+      </AwesomeButton> */}
+    </>
+  );
 }
